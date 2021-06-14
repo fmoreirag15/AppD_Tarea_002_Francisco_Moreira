351f4503ef66ca9c8af69a3038fff0effc8998c0
fmoreirag15 <francisco.moreira2017@uteq.edu.ec>
2021-06-14 03:55:33 -0500

    m

---
+++ app/src/main/java/com/example/segundodeber/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.segundodeber;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.segundodeber.modelo.adaptadorLista;
+import com.example.segundodeber.modelo.modelos_revista;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import  android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtBuscar;
+    TextView txtInfor;
+    Button btnConsultar;
+    //RecyclerView recyclerView2;
+    List<modelos_revista> datos= new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtBuscar = findViewById(R.id.editTextNumber);
+        btnConsultar=findViewById(R.id.button);
+        btnConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtBuscar = findViewById(R.id.editTextNumber);
+                obtenerDatosConVolley(txtBuscar.getText().toString());
+            }
+        });
+        obtenerDatosConVolley("1");
+    }
+    private void obtenerDatosConVolley(String codigo)
+    {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url="https://revistas.uteq.edu.ec/ws/issues.php?j_id="+codigo+"";
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        if(response.length()>0) {
+                            datos.clear();
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    JSONObject Obj = response.getJSONObject(i);
+                                    //datos.add(new  modelos_revista("sss","dsads","asda","asdasd","adasd","asdasd","asdad","https://revistas.uteq.edu.ec/public/journals/1/cover_issue_80_es_ES.png"));
+                                    System.out.println("titulos"+Obj.get("title").toString());
+
+                                    datos.add(new modelos_revista(  Obj.get("issue_id").toString(), Obj.get("volume").toString(),
+                                                                    Obj.get("number").toString(),
+                                                                    Obj.get("year").toString(),
+                                                                    Obj.get("date_published").toString(),
+                                                                    Obj.get("title").toString(),
+                                                                    Obj.get("doi").toString(),
+                                                                    Obj.get("cover").toString()));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+
+
+                        }else
+                        {
+                            mensajes("No se encontraron resultados");
+
+
+                        }
+                        ejecutar();
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        mensajes("Ocurrio un error");
+                        error.printStackTrace();
+
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+
+    }
+    public void  mensajes(String mensaje)
+    {
+        Toast.makeText(this,mensaje,Toast.LENGTH_LONG ).show();
+    }
+    public void ejecutar()
+    {
+        if(datos.size()<1)
+        {
+            mensajes("No se encontraron resultados");
+            txtInfor=findViewById(R.id.textView3);
+            txtInfor.setText("Resultados de la consulta: "+datos.size());
+
+        }else
+        {
+            mensajes("Lista de resultados");
+            txtInfor=findViewById(R.id.textView3);
+            txtInfor.setText("Resultados de la consulta: "+datos.size());
+        }
+        adaptadorLista lista= new adaptadorLista(datos, this);
+        RecyclerView recyclerView=findViewById(R.id.listaRevista);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(lista);
+
+    }
+}
---
+++ app/src/main/java/com/example/segundodeber/modelo/adaptadorLista.java
@@ -0,0 +1,69 @@
+package com.example.segundodeber.modelo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.segundodeber.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+public class adaptadorLista extends RecyclerView.Adapter<adaptadorLista.ViewHolder> {
+    private List<modelos_revista> Data;
+    private LayoutInflater myinflater;
+    private Context context;
+
+    public adaptadorLista(List<modelos_revista> itemList, Context context)
+    {
+        this.myinflater=LayoutInflater.from(context);
+        this.context=context;
+        this.Data=itemList;
+    }
+    @Override
+    public  int getItemCount()
+    { return Data.size();
+         }
+    @Override
+    public adaptadorLista.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        View view=myinflater.inflate(R.layout.lista_de_elementos, null);
+        return new adaptadorLista.ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(final  adaptadorLista.ViewHolder holder, final int position)
+    {
+        holder.bindData(Data.get(position));
+
+    }
+    public  void  setItems(List<modelos_revista> items){Data=items;}
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageRevista;
+        TextView titulo, volumen, data, doi, anio, cover;
+        ViewHolder(View itemView)
+        {
+            super(itemView);
+            imageRevista=itemView.findViewById(R.id.imagenViewlista);
+            titulo=itemView.findViewById(R.id.txtTitulo);
+            volumen=itemView.findViewById(R.id.Txtvolumen);
+            data=itemView.findViewById(R.id.txtdate_published);
+            doi=itemView.findViewById(R.id.txtDoi);
+            anio=itemView.findViewById(R.id.Txtanio);
+
+        }
+        void bindData(final  modelos_revista item)
+        {
+            Picasso.get().load(item.getCover()).resize(100,100).centerCrop().into(imageRevista);
+            titulo.setText(item.getTitle());
+            volumen.setText(item.getVolume());
+            data.setText(item.getDate_published());
+            doi.setText(item.getDoi());
+            anio.setText(item.getYear());
+        }
+    }
+
+
+}
